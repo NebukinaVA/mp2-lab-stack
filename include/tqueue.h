24c1ba7abe2c55b4f1998c4a281b02046e2d7a17
Nebukina Viktoria <43473978+NebukinaVA@users.noreply.github.com>
2019-11-14 22:18:58 +0300

    queue added

---
+++ include/tqueue.h
@@ -0,0 +1,71 @@
+#ifndef __TQUEUE_H__
+#define __TQUEUE_H__
+
+#include <iostream>
+#include "tstack.h"
+
+using namespace std;
+
+// ОЧЕРЕДЬ
+template <class ValType>
+class TQueue :public TStack<ValType>
+{
+	TStack<ValType> In, Out;
+public:
+	TQueue(int s = 6);
+	~TQueue();
+	void Pop();                         // убрать элемент
+	void Push(ValType elem);            // добавить элемент 
+	bool operator==(const TQueue &q);   // сравнение
+	bool operator!=(const TQueue &q);   // сравнение
+};
+
+template <class ValType>
+TQueue<ValType>::TQueue(int s)
+{
+	TStack <ValType> temp(s);
+	In = temp;
+	Out = temp;
+}
+
+template <class ValType>
+TQueue<ValType>::~TQueue()
+{
+}
+
+template <class ValType>
+void TQueue<ValType>::Pop()
+{
+	if (!In.IsEmpty())
+		Out = In.LoadStack();
+	Out.Pop();
+	In.Clear();
+}
+
+template <class ValType>
+void TQueue<ValType>::Push(ValType elem)
+{
+	if (!Out.IsEmpty())
+		In = Out.LoadStack();
+	In.Push(elem);
+	Out.Clear();
+}
+
+template <class ValType>
+bool TQueue<ValType>::operator==(const TQueue &q) //сравнение
+{
+	if ((!q.In.IsEmpty()) && (!In.IsEmpty()))
+		if (In == q.In) return true;
+	if ((!q.Out.IsEmpty()) && (!Out.IsEmpty()))
+		if (Out == q.Out) return true;
+	return false;
+}
+
+template <class ValType>
+bool TQueue<ValType>::operator!=(const TQueue &q) //сравнение
+{
+	if (q == *this) return false;
+	return true;
+}
+
+#endif
